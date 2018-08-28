b5fb711bfb7a40bb7e31cf9b1c7ffd762b79f645
Filipe Fernandes <ocefpaf@gmail.com>
2018-08-28 15:38:07 -0300

    patch proj datadir

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,5 +1,11 @@
+import os
+import sys
 import pyproj
 pyproj.test()
 
 from pyproj import Proj
 Proj(init='epsg:4269')
+
+# Test pyproj_datadir
+if not os.path.isdir(os.path.join(pyproj.pyproj_datadir)):
+    sys.exit(1)

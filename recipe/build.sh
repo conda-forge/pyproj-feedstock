4727e3b08f38083f99291d1d895c8c8339a88ab7
Xylar Asay-Davis <xylarstorm@gmail.com>
2024-06-07 21:22:01 +0200

    Add comment

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,4 @@
+# based on https://github.com/conda-forge/singlejar-feedstock/blob/e76b380372282bc9df5c27faec8ca9149e0fdcc4/recipe/build.sh#L9
 export PROJ_VERSION=$(conda list -p $PREFIX proj | grep -v '^#' | tr -s ' ' | cut -f 2 -d ' ')
 echo "PROJ_VERSION detected as ${PROJ_VERSION}"
 
